e90a7a8ffe01ca3cfbe7feb2407af73929293613
Noslen <ludonelvic@gmail.com>
2023-10-20 18:45:07 +0100

    the logos have been added and the dropdown done

--- src/App.js
+++ src/App.js
@@ -92,11 +92,13 @@ export default function App() {
       }
         <div className="header">
           <div className='logoContainer'>
-            <img src='/logo192.png' alt='ccousp logo' className='ccouspLogo'/>
-            <h6>Ccousp & app name</h6>
+            <img src='/cems_logo.png' alt='ccousp logo' className='ccouspLogo'/>
+            <h6>Ccousp Portal App</h6>
           </div>
           <div className="searchBar">
-              <img src="" alt="🔎"/>
+              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+              </svg>
               <input className="searchBarInput" placeholder="search you apps"/>
           </div>
         </div>
--- src/components/Sections.js
+++ src/components/Sections.js
@@ -26,10 +26,22 @@ export default function Sections({sectionList, setSectionList}){
             <div className="allSectionsContainer">
             {sectionList.map((section, index) => ( /**mapping through the sectionList array to access all the section objects which are elements in this array */
                     <div key={index} className="singleSectionContainer">
-                        <button className="sectionDropDownBtn" onClick={() => toggleSection(index)}><img src="" alt="^"/>{section.title}</button>
+                        <button className="sectionDropDownBtn" onClick={() => toggleSection(index)}>
+                            {isActiveList[index]? 
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                                    <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM377 271c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-87-87-87 87c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9L239 167c9.4-9.4 24.6-9.4 33.9 0L377 271z"/>
+                                </svg> :
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                                    <path d="M256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM135 241c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l87 87 87-87c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9L273 345c-9.4 9.4-24.6 9.4-33.9 0L135 241z"/>
+                                </svg> 
+                            }
+                            {section.title}
+                        </button>
                         <button onClick={() => {
                             setSectionList(sectionList.filter(f => f.title !== section.title));}} className="appOptionsButton" > {/**include every element in the new array created by the filter except the element with the same title */}
-                            🗑️
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                                <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/>
+                            </svg>
                         </button>
                         {isActiveList[index] && 
                             <AppSection sectionApps={section.apps} handleDeleteAppClick={handleDeleteAppClick}/>
@@ -47,7 +59,9 @@ function AppSection({sectionApps, handleDeleteAppClick}){
             {sectionApps.map((app,index) => ( /** mapping through the single section apps array to get all apps in one section */
                     <div key={index} className="app">
                         <button className="appOptionsButton" onClick={() => handleDeleteAppClick(app.title)}> {/**include every element in the new array created by the filter except the element with the same title */}
-                        🗑️
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                                    <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/>
+                            </svg>
                         </button>{/**this is temporarily the delete app button i shall add those in the "..." of the app button */}
                         <img className="appImage" src="/logo192.png"  alt="this is the app"/> {/*app.image*/}
                         <p>{app.title}</p>

670d79cd7b562103aa21576033f7339efc3647fe
Noslen <ludonelvic@gmail.com>
2024-03-21 11:42:24 +0100

    error messages and logo updated

--- src/components/ErrorMsg.js
+++ src/components/ErrorMsg.js
@@ -1,23 +1,28 @@
 import React, { useEffect } from "react";
 import "../css/errorMsg.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faExclamationTriangle,
+  faCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
 
 const ErrorMsg = ({ errorMsg }) => {
   return (
-    <div className="errContainer">
-      <div
-        className={
-          errorMsg?.display
-            ? errorMsg?.good
-              ? "errorMsg errorMsg-show good"
-              : "errorMsg errorMsg-show"
-            : "errorMsg errorMsg-hide"
-        }
-      >
+    <div
+      className={
+        errorMsg?.display
+          ? errorMsg?.good
+            ? "errorMsg errorMsg-show good"
+            : "errorMsg errorMsg-show"
+          : "errorMsg errorMsg-hide"
+      }
+    >
+      {errorMsg?.good ? (
+        <FontAwesomeIcon icon={faCircleCheck} size="2x" />
+      ) : (
         <FontAwesomeIcon icon={faExclamationTriangle} size="2x" />
-        <h6>{errorMsg?.msg}</h6>
-      </div>
+      )}
+      <h6>{errorMsg?.msg}</h6>
     </div>
   );
 };
